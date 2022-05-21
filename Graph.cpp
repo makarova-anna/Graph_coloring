af6ca548dc6b8f99442c055cd094d83b59a94e6e
makarova-anna <104019453+makarova-anna@users.noreply.github.com>
2022-05-21 23:38:07 +0300

    update 2

--- Graph.cpp
+++ Graph.cpp
@@ -1,4 +1,4 @@
-#include "Header.h"
+#include "Graph.h"
 
 
 void Graph::Add_edge(int u, int v)
@@ -38,6 +38,54 @@ void Graph::Coloring()
         Size_smeg[i] = Smeg[i].size();
 
     sort(Size_smeg.begin(), Size_smeg.end(), std::greater<int>());
-    
+
+
+    int count = 0;
+    int w = 0;
+    set <int> used_column;  
+    while (count < N_vertex)
+    {
+        for (w = 0; w< N_vertex; w++)
+        {
+            auto search = used_column.find(w);
+            if (Smeg[w].size() == Size_smeg[count] && inside(used_column, w) == 0)
+            {
+                used_column.insert(w);
+                break;
+            }
+            //cout << "nice" << endl;
+        }
+
+        auto iter = Smeg[w].begin();
+        while (iter != Smeg[w].end())
+        {
+            if (Colors[*iter] != -1)
+                is_available[Colors[*iter]];
+            iter++;
+        }
+        //прроходим по всем веришнам, соединенным с i-й
+        for (iter = Smeg[w].begin(); iter != Smeg[w].end(); iter++)  
+            if (Colors[*iter] != -1)
+                is_available[Colors[*iter]] = true;
+
+        // Ищем первый доступный нам цвет
+        int current;
+        for (current = 0; current < N_vertex; current++)
+            if (is_available[current] == false)
+                break;
+
+        Colors[w] = current; // добавляем в массив
+
+        // Для следующей итерации меняем
+        for (iter = Smeg[w].begin(); iter != Smeg[w].end(); iter++)
+            if (Colors[*iter] != -1)
+                is_available[Colors[*iter]] = false;
+
+        count++;
+        w = 0;
+    }
+
+
+
 
 }
